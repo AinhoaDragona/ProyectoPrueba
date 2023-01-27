fb80aaf70c6bec74d13b513e27c5b7faa562c9d8
infazorrilla <infazorrilla@HZ076028.elorrieta.int>
2023-01-27 16:41:00 +0100

    3ª version de Ana

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		System.out.println("Estamos empezando el proyecto con Git.");
 		System.out.println("1ª modificación del proyecto.");
 		System.out.println("2ª modificación en proyecto Ainhoa.");
+		System.out.println("A ver si funciona");
 	}
 
 }
