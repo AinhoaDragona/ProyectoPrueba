3dd34d64496f84c9e1dc7146a7177bdafdaa0566
infazorrilla <infazorrilla@HZ076028.elorrieta.int>
2023-01-27 16:21:54 +0100

    Modificacion de Ana

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos empezando el proyecto con Git.");
 		System.out.println("1ª modificación del proyecto.");
+		System.out.println("2ª modificación en proyecto Ainhoa.");
 	}
 
 }
